35a7ae2fb8c85dc7f068f221aefe7244c44f6272
Shivam Tiwari <synoep@gmail.com>
2025-06-17 16:57:16 +0530

    f

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
+
 import leaderboardRoutes from './leaderboard.routes.js';
 
-dotenv.config();
+
 
 const app = express();
-
+console.log('mongodb://localhost:27017/leaderboard');
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect('mongodb://localhost:27017/leaderboard', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
@@ -29,7 +31,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 }); 
